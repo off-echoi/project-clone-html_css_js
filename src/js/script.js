5b45fb4234f8149c6bfebdc223a7dce915afd6da
ecchoi <off.echoi@gmail.com>
2021-07-26 23:40:43 +0900

    Add 공통 부분 추가, 푸터 스타일 적용, 요리재료 스와이퍼 적용(js, css)

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,16 @@
 console.log('test');
+
+// food_materials 요리재료 스와이퍼 테스트
+const swiper = new Swiper('.swiper-container', {
+  // loop: true,
+  slidesPerView: 5,
+  slidesPerGroup: 5,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
