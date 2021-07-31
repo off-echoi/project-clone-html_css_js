5460d98482a19498fba96a215fae42102a1915f0
ecchoi <off.echoi@gmail.com>
2021-07-31 17:45:11 +0900

    Add media 스와이퍼 적용(임시)

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,15 @@
 console.log('test');
 
+// media_container 요리재료 스와이퍼 테스트
+const swiper1 = new Swiper('.media_container', {
+  loop: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  spaceBetween: 30,
+});
+
 // food_materials 요리재료 스와이퍼 테스트
-const swiper = new Swiper('.swiper-container', {
+const swiper2 = new Swiper('.materials_container', {
   // loop: true,
   slidesPerView: 5,
   slidesPerGroup: 5,
