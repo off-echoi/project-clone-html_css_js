c83aa125d2384952f406a9e92cb58385d242eb4d
ecchoi <off.echoi@gmail.com>
2021-08-01 16:16:09 +0900

    Add media쪽 스와이퍼 인디테이터 추가

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,18 @@
 console.log('test');
 
-// media_container 요리재료 스와이퍼 테스트
+// media_container media 스와이퍼 테스트
 const swiper1 = new Swiper('.media_container', {
   loop: true,
   centeredSlides: true,
   slidesPerView: 'auto',
   spaceBetween: 30,
+  pagination: {
+    el: '.media_pagination',
+    renderCustom: function (swiper, current, total) {
+      return `<div>${current} / ${total}</div>`;
+    },
+    type: 'custom',
+  },
 });
 
 // food_materials 요리재료 스와이퍼 테스트
